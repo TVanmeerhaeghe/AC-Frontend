2a11552bf676009326923b43e0df4a780f744339
Gaëtan <dapvril.gaetan@gmail.com>
2025-04-07 16:37:24 +0200

    style: :lipstick: Prepared menu style for navigation

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../../navigation/menu/menu.component';
+// import { FooterComponent } from '../../navigation/footer/footer.component'; FooterComponent
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [MenuComponent,],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
