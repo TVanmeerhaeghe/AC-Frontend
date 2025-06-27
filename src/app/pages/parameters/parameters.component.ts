f1a44a9a2e54fe0c560cef65d0fbe9ecc76e7285
Gaëtan <dapvril.gaetan@gmail.com>
2025-06-27 15:47:25 +0200

    style: :lipstick: Improved the style

--- src/app/pages/parameters/parameters.component.ts
+++ src/app/pages/parameters/parameters.component.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { User } from '../../models/users.model';
+import { ConfirmPopupComponent } from '../../shared/confirm-popup/confirm-popup.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-parameters',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ConfirmPopupComponent],
   templateUrl: './parameters.component.html',
-  styleUrl: './parameters.component.scss'
+  styleUrls: ['./parameters.component.scss']
 })
-export class ParametersComponent {
+export class ParametersComponent implements OnInit {
+  user?: User;
+  loadingUser = true;
+  showConfirm = false;
+  confirmTitle = '';
+  confirmMessage = '';
+  confirmAction: (() => void) | null = null;
 
+  constructor(private api: ApiService, private snackBar: MatSnackBar) {}
+
+  ngOnInit() {
+    this.loadingUser = true;
+    // Récupère l'utilisateur courant depuis le localStorage (clé à adapter si besoin)
+    const userId = Number(localStorage.getItem('userId'));
+    if (userId && !isNaN(userId)) {
+      this.api.getUser(userId).subscribe({
+        next: user => {
+          this.user = user;
+          this.loadingUser = false;
+        },
+        error: () => {
+          this.user = undefined;
+          this.loadingUser = false;
+        }
+      });
+    } else {
+      this.user = undefined;
+      this.loadingUser = false;
+    }
+  }
+
+  askDeleteAccount() {
+    this.confirmTitle = 'Supprimer mon compte ?';
+    this.confirmMessage = 'Cette action est définitive et supprimera toutes vos données.';
+    this.confirmAction = () => {
+      if (this.user?.id) {
+        this.api.deleteUser(this.user.id).subscribe({
+          next: () => {
+            this.snackBar.open('Compte supprimé avec succès', 'Fermer', { duration: 3000 });
+            // Rediriger ou déconnecter l'utilisateur ici si besoin
+          },
+          error: () => {
+            this.snackBar.open('Erreur lors de la suppression du compte', 'Fermer', { duration: 3000 });
+          }
+        });
+      }
+    };
+    this.showConfirm = true;
+  }
+
+  onConfirmPopup() {
+    if (this.confirmAction) this.confirmAction();
+    this.showConfirm = false;
+  }
+  onCancelPopup() {
+    this.showConfirm = false;
+  }
 }
